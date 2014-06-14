85d72b18286e3ab401f7bc5a3c452fe0e732ae75
Simon Taranto <simon.taranto@gmail.com>
2014-06-14 15:54:03 -0700

    Add fetching of API KEY from env var

--- dailyweather.go
+++ dailyweather.go
@@ -3,20 +3,27 @@ package main
 import (
   "fmt"
   forecast "github.com/mlbright/forecast/v2"
-  "log"
+  "os"
 )
 
 func main() {
-  key := "xxx"
+  key := os.Getenv("FORECAST_IO_API_KEY")
 
-  lat := "35"
-  long := "-122"
+  lat := "37.7833"
+  long := "-122.4167"
 
   f, err := forecast.Get(key, lat, long, "now", forecast.US)
   if err != nil {
-    log.Fatal(err)
+    fmt.Printf("Error occurred in API call: %s", err)
   }
 
-  fmt.Printf("%s\n", f.Currently.Summary)
-  fmt.Printf("%.2f", f.Currently.Temperature)
+  fmt.Printf("Summary: %s", f.Currently.Summary)
+  fmt.Printf("\n")
+  fmt.Printf("Temperature: %.2f", f.Currently.Temperature)
+  fmt.Printf("\n")
+  fmt.Printf("Cloud Cover: %.2f", f.Currently.CloudCover)
+  fmt.Printf("\n")
+  fmt.Printf("Humidity: %.2f", f.Currently.Humidity)
+  fmt.Printf("\n")
+  fmt.Printf("Visibility: %.2f", f.Currently.Visibility)
 }
